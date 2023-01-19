9dbf6a06edd5c67d1ba0e657bafc342c7bd20aa9
Nathaniel Foster <nathanielfosterco@gmail.com>
2023-01-19 07:52:23 -0700

    Add socket.io and connect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,27 @@
 import express from 'express';
 import path from 'path';
-
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
 const port = 3001;
+const io = new Server(server);
 
 app.use(express.static(path.resolve(__dirname, '../../website/build')));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('listening on *:3001');
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../website/build', 'index.html'));
 });
+
+io.on('connection', (socket) => {
+  console.log('connected!');
+})
--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
+import { io } from "socket.io-client";
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  })
+
   return (
     <div className="App">
       <header className="App-header">        
-        <form>
           <label htmlFor="name-input">Welcome To Toucan Party Chat, What Is Your Name?</label>
+          <p>{!data ? "Loading..." : data}</p>
           <input type="text" name="name-input" id="name-input" />
-          <button>Save</button>
-        </form>
+          <button onClick={() => {
+            const socket = io('http://localhost:3001');
+          }}>Save</button>
       </header>
     </div>
   );
