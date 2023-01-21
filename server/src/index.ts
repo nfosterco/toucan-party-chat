042e4852fd027331cc42b385c860c35b9b246f5f
nathaniel foster <nathanielfosterco@gmail.com>
2023-01-21 08:11:52 -0700

    Remove extra endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,10 +17,6 @@ server.listen(port, () => {
   console.log('listening on *:3001');
 });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../website/build', 'index.html'));
 });
